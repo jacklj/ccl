d1406670fb433bda85c588bd637dbd7ad5f2fb5b
Jack Lawrence-Jones <jackvlj@gmail.com>
2016-08-02 13:01:06 +0100

    2d arrays now handled by numpy

--- ccl.py
+++ ccl.py
@@ -11,6 +11,8 @@
 """
 
 import copy
+import numpy as np
+# import Image
 from union_find import MakeSet, Union, Find, getNode, display_all_sets
 
 
@@ -18,40 +20,45 @@ CONNECTIVITY_4 = 1
 CONNECTIVITY_8 = 2
 
 
-def connected_component_labelling(image, connectivity_type):
+def connected_component_labelling(bool_input_image, connectivity_type):
 	"""
 		2 pass algorithm using disjoint-set data structure to maintain record of label 
 		equivalences.
+
+		Input: binary image as 2D boolean array.
+		Output: 2D integer array of labelled pixels.
 
 		1st pass: label image and record label equivalence classes.
 		2nd pass: replace labels with their root labels.
 
 		(optional 3rd pass: Flatten labels so they are consecutive integers starting from 1.)
 
-		Takes monochrome image represented as 2D integer array:
-			foreground pixels -> 1
-			background pixels -> 0
-
 	"""
-	image = copy.deepcopy(image) # Copy input image so we don't change the input image itself.
+	image_width = len(bool_input_image[0])
+	image_height = len(bool_input_image)
+
+	# initialise efficient 2D int array with numpy
+	# N.B. numpy matrix addressing syntax: array[y,x]
+	labelled_image = np.zeros((image_height, image_width), dtype=np.int16)
+
 	current_label = 1 # Label counter
 
 	# 1st Pass: label image and record label equivalences.
-	for y, row in enumerate(image): # Pythonic loop index variables
+	for y, row in enumerate(bool_input_image): # Pythonic loop index variables
 		for x, pixel in enumerate(row):
 			
 			if pixel <= 0:
-				# Background pixel - do nothing
+				# Background pixel - leave output pixel value as 0
 				pass
 			else: 
 				# Foreground pixel - work out what its label should be.
 
 				# Get set of neighbour's labels
-				labels = neighbouring_labels(image, connectivity_type, x, y)
+				labels = neighbouring_labels(labelled_image, connectivity_type, x, y)
 
 				if not labels:
 					# If no neighbouring foreground pixels, new label - use current_label 
-					image[y][x] = current_label
+					labelled_image[y,x] = current_label
 					MakeSet(current_label) # record label in disjoint set
 					current_label = current_label + 1 # increment for next time				
 				
@@ -59,7 +66,7 @@ def connected_component_labelling(image, connectivity_type):
 					# Pixel is definitely part of a connected component: get smallest label of 
 					# neighbours
 					smallest_label = smallest(labels)
-					image[y][x] = smallest_label
+					labelled_image[y,x] = smallest_label
 
 					if len(labels) > 1: # More than one type of label in component -> add 
 										# equivalence class.
@@ -71,13 +78,13 @@ def connected_component_labelling(image, connectivity_type):
 	final_labels = {}
 	new_label_number = 1
 
-	for y, row in enumerate(image):
+	for y, row in enumerate(labelled_image):
 		for x, pixel_value in enumerate(row):
 			
 			if pixel_value > 0: # Foreground pixel
 				# Get element's set's representative value and use as the pixel's new label.
 				new_label = Find(getNode(pixel_value)).value 
-				image[y][x] = new_label
+				labelled_image[y,x] = new_label
 
 				# Add label to list of labels used, for 3rd pass (flattening label list).
 				if new_label not in final_labels.keys():
@@ -88,13 +95,13 @@ def connected_component_labelling(image, connectivity_type):
 	# 3rd Pass: flatten label list so labels are consecutive integers starting from 1 (in order 
 	# of top to bottom, left to right).
 	# Different implementation of disjoint-set may remove the need for 3rd pass?
-	for y, row in enumerate(image):
+	for y, row in enumerate(labelled_image):
 		for x, pixel_value in enumerate(row):
 			
 			if pixel_value > 0: # Foreground pixel
-				image[y][x] = final_labels[pixel_value]
+				labelled_image[y,x] = final_labels[pixel_value]
 
-	return image
+	return labelled_image
 
 
 
@@ -120,13 +127,13 @@ def neighbouring_labels(image, connectivity_type, x, y):
 	if connectivity_type == CONNECTIVITY_4 or connectivity_type == CONNECTIVITY_8:
 		# West neighbour
 		if x > 0: # pixel is not on left edge of image
-			west_neighbour = image[y][x-1]
+			west_neighbour = image[y,x-1]
 			if west_neighbour > 0: # it's a labelled pixel
 				labels.add(west_neighbour)
 
 		# North neighbour
 		if y > 0: # pixel is not on top edge of image
-			north_neighbour = image[y-1][x]
+			north_neighbour = image[y-1,x]
 			if north_neighbour > 0: # it's a labelled pixel
 				labels.add(north_neighbour)
 
@@ -134,13 +141,13 @@ def neighbouring_labels(image, connectivity_type, x, y):
 	if connectivity_type == CONNECTIVITY_8:
 		# North-west neighbour
 		if x > 0 and y > 0: # pixel is not on left or top edges of image
-			northwest_neighbour = image[y-1][x-1]
+			northwest_neighbour = image[y-1,x-1]
 			if northwest_neighbour > 0: # it's a labelled pixel
 				labels.add(northwest_neighbour)
 
 		# North-east neighbour
 		if y > 0 and x < len(image[y]) - 1: # pixel is not on top or right edges of image
-			northeast_neighbour = image[y-1][x+1]
+			northeast_neighbour = image[y-1,x+1]
 			if northeast_neighbour > 0: # it's a labelled pixel
 				labels.add(northeast_neighbour)
 
@@ -175,10 +182,10 @@ def print_image(image):
 # Testing ######################################################################################
 # to do: proper test cases
 i1 = [
-	[0,0,0,0],
-	[0,1,1,1],
-	[0,0,0,1],
-	[0,1,1,1],
+	[False,False,False,False],
+	[False,True,True,True],
+	[False,False,False,True],
+	[False,True,True,True],
 	]
 
 i2 = [
@@ -195,12 +202,12 @@ i2 = [
 	]
 
 print("Original image:")
-print_image(i2)
+print_image(i1)
 print("----")
 
-result = connected_component_labelling(i2, CONNECTIVITY_4)
+result = connected_component_labelling(i1, CONNECTIVITY_4)
 print("Labelled:")
-print_image(result)
+print(result)
 
 
 # test smallest(set) function
